87ae63f043138814a9175522bd54a4d3112328b0
David <albein81000@gmail.com>
2022-11-19 19:08:26 +0530

    join group page created!

--- web-front-end/src/Pages/Groups/CreateGroup.js
+++ web-front-end/src/Pages/Groups/CreateGroup.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./CreateGroup.css";
 import {Link} from "react-router-dom";
 export default function CreateGroup() {
   return (
--- web-front-end/src/Pages/Groups/JoinGroup.js
+++ web-front-end/src/Pages/Groups/JoinGroup.js
@@ -1,7 +1,22 @@
 import React from 'react'
-
+import "./JoinGroup.css";
+import {Link} from "react-router-dom";
 export default function JoinGroup() {
   return (
-    <div>JoinGroup</div>
+    <div id='Body'>
+    <div className="card" id='appCard'>
+      <div className="card-body">
+        <h5 className="card-title" id='title'>Join Group</h5>
+        <h6 className="card-subtitle mb-2 text-muted" id='subTitle'>Enter Group Name & Password</h6>
+        <div class="input-group flex-nowrap" id="userName">
+          <input type="text" class="form-control" placeholder="Groupname" aria-label="Username" aria-describedby="addon-wrapping"/>
+        </div>
+        <div class="input-group flex-nowrap" id="passWord">
+          <input type="password" class="form-control" placeholder="Password" aria-label="Username" aria-describedby="addon-wrapping"/>
+        </div>
+        <Link to="/groups"><button type="button" class="btn btn-primary btn-lg btn-block" id='join-btn'>Join</button></Link>
+      </div>
+    </div>
+    </div>
   )
 }
