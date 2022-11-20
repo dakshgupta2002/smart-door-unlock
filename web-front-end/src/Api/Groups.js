160fcaace140da8c418e497417e7ccad8a168334
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-11-20 13:43:56 +0530

    pass auth to all from props

--- web-front-end/src/Api/Groups.js
+++ web-front-end/src/Api/Groups.js
@@ -1,20 +1,30 @@
-import { db } from "../firebase";
-import { collection, doc, setDoc } from "firebase/firestore";
+import { auth, db } from "../firebase";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 // Create group
-export const createGroupApi = async () => {
-    await setDoc( doc(db, 'groups', 'one'), {
-        admin: '123',
-        freeze: 'false',
-        server: '123.456.789'
-    })
+export const createGroupApi = async (email, gname, serverID) => {
+  //verify if server exists
+  const docSnap = await getDoc(doc(db, "servers", serverID));  
+  if (docSnap.exists()){
+   
+    // await setDoc(doc(db, 'groups', gname), {
+    //   admin: email,
+    //   freeze: 'false',
+    //   status: 'false',
+
+    // })
+  }
+  else {
+    toast.error("Server id is invalid")
+  }
 }
 // Join group
 export const joinGroupApi = () => {
-    
+
 }
 
 // My Groups
 export const fetchMyGroupsApi = async () => {
-    
+
 }
--- web-front-end/src/Pages/Groups/CreateGroup.js
+++ web-front-end/src/Pages/Groups/CreateGroup.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { createGroupApi } from '../../Api/Groups';
 import "./CreateGroup.css";
-import {Link} from "react-router-dom";
+
 export default function CreateGroup() {
+  const [gname, setGName] = useState("");
+  const [serverID, setServerID] = useState("");
+  
+  const createGroup = async () => {
+    await createGroupApi(gname, serverID)
+  }
+
   return (
     <div id='Body'>
     <div className="card" id='appCard'>
@@ -9,15 +17,13 @@ export default function CreateGroup() {
         <h5 className="card-title" id='title'>Create Group</h5>
         <h6 className="card-subtitle mb-2 text-muted" id='subTitle'>Enter Group Name, Server-ID & Password</h6>
         <div class="input-group flex-nowrap" id="userName">
-          <input type="text" class="form-control" placeholder="Groupname" aria-label="Username" aria-describedby="addon-wrapping"/>
+          <input type="text" class="form-control" placeholder="Group Name" aria-label="groupname" aria-describedby="addon-wrapping" value={gname} onChange={(e) => {setGName(e.target.value)}} />
         </div>
         <div class="input-group flex-nowrap" id="serverID">
-          <input type="password" class="form-control" placeholder="Server-ID" aria-label="Username" aria-describedby="addon-wrapping"/>
+          <input type="text" class="form-control" placeholder="Server-ID" aria-label="serverId" aria-describedby="addon-wrapping" value={serverID} onChange={(e) => {setServerID(e.target.value)}} />
         </div>
-        <div class="input-group flex-nowrap" id="passWord">
-          <input type="password" class="form-control" placeholder="Password" aria-label="Username" aria-describedby="addon-wrapping"/>
-        </div>
-        <Link to="/groups"><button type="button" class="btn btn-primary btn-lg btn-block" id='create-btn'>Create</button></Link>
+
+        <button type="button" class="btn btn-primary btn-lg btn-block" id='create-btn' onClick={createGroup}>Create</button>
       </div>
     </div>
     </div>
