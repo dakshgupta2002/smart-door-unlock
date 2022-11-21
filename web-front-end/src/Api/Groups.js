14d256b87860f16651a2c9496152073db0e4ee51
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-11-21 10:16:10 +0530

    enter key and join group

--- web-front-end/src/Api/Groups.js
+++ web-front-end/src/Api/Groups.js
@@ -1,35 +1,32 @@
 import { db } from "../firebase";
-import { collection, doc, getDoc, setDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { toast } from "react-toastify";
 import { v4 as uuidv4 } from 'uuid';
 
-// Create group
-export const createGroupApi = async (email, gname, serverID) => {
-  //verify if server exists
+export const createGroupApi = async (email, gname, serverID) => { //otp verification
   if (!serverID) {
     toast.error("Server ID failed to be provided")
     return;
   }
   const serverSnap = await getDoc(doc(db, "servers", serverID));
-  if (serverSnap.exists()) {
+  if (serverSnap.exists()) { //verify if this server exists
     if (serverSnap.data().available === true) {
-      // this server cannot create a new group
+      // this server will not create a new group
       await setDoc(doc(db, "servers", serverID), { available: false }, { merge: true });
-      //generate the secret key for the group
-      const key = uuidv4();
-      //create a new group using this server
-      await setDoc(doc(db, "groups", key), {
+      const key = uuidv4(); //generate the secret key for the group
+      
+      await setDoc(doc(db, "groups", serverID), { //create a new group using this server
         name: gname,
         admin: email,
         freeze: 'false',
         status: 'false',
         history: [],
-        server: serverID,
+        key: key
       })
-      // add the grp id to user doc 
+      // add the grp id to the user doc 
       const userRef = await getDoc(doc(db, "users", email));
       const updatedGroups = userRef.data().groups;
-      updatedGroups.push(key);
+      updatedGroups.push(serverID);
       await setDoc(doc(db, "users", email), {groups: updatedGroups}, {merge: true});
       // display the changes to the user
       toast.success("Group has been created");
@@ -43,8 +40,29 @@ export const createGroupApi = async (email, gname, serverID) => {
   }
 }
 // Join group
-export const joinGroupApi = () => {
-
+export const joinGroupApi = async (email, serverID, key) => {
+  const userRef = await getDoc(doc(db, "users", email));
+  if (userRef.data().groups.includes(serverID)){
+    toast.info("You are already in this group")
+    window.location.href = `/groups/${serverID}`
+    return;
+  }
+  // check if the key exists
+  if (!key || !serverID) {
+    toast.error("Key failed to be provided")
+    return;
+  }
+  const groupSnap = await getDoc(db, "groups", serverID);
+  if (groupSnap.exists()) { //verify if they server id matches
+    if (groupSnap.data().key === key) {
+      //join the group, add the user to the group
+      const updatedGroups = userRef.data().groups;
+      updatedGroups.push(serverID);
+      await setDoc(doc(db, "users", email), {groups: updatedGroups}, {merge: true});
+    }else{
+      toast.error("The key is invalid")
+    }
+  }else toast.error("The credentials provided are wrong")
 }
 
 // My Groups
--- web-front-end/src/Pages/Groups/CreateGroup.js
+++ web-front-end/src/Pages/Groups/CreateGroup.js
@@ -16,14 +16,14 @@ export default function CreateGroup(props) {
       <div className="card-body">
         <h5 className="card-title" id='title'>Create Group</h5>
         <h6 className="card-subtitle mb-2 text-muted" id='subTitle'>Enter Group Name, Server-ID & Password</h6>
-        <div class="input-group flex-nowrap" id="userName">
-          <input type="text" class="form-control" placeholder="Group Name" aria-label="groupname" aria-describedby="addon-wrapping" value={gname} onChange={(e) => {setGName(e.target.value)}} />
+        <div className="input-group flex-nowrap" id="userName">
+          <input type="text" className="form-control" placeholder="Group Name" aria-label="groupname" aria-describedby="addon-wrapping" value={gname} onChange={(e) => {setGName(e.target.value)}} />
         </div>
-        <div class="input-group flex-nowrap" id="serverID">
-          <input type="text" class="form-control" placeholder="Server-ID" aria-label="serverId" aria-describedby="addon-wrapping" value={serverID} onChange={(e) => {setServerID(e.target.value)}} />
+        <div className="input-group flex-nowrap" id="serverID">
+          <input type="text" className="form-control" placeholder="Server-ID" aria-label="serverId" aria-describedby="addon-wrapping" value={serverID} onChange={(e) => {setServerID(e.target.value)}} />
         </div>
 
-        <button type="button" class="btn btn-primary btn-lg btn-block" id='create-btn' onClick={createGroup}>Create</button>
+        <button type="button" className="btn btn-primary btn-lg btn-block" id='create-btn' onClick={createGroup}>Create</button>
       </div>
     </div>
     </div>
