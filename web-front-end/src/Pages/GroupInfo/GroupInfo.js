464ea1d1dc32cc44b39bce0cc180176c00e1b051
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-11-28 17:28:58 +0530

    admin freeze and unfreeze

--- web-front-end/src/Pages/GroupInfo/GroupInfo.js
+++ web-front-end/src/Pages/GroupInfo/GroupInfo.js
@@ -5,10 +5,18 @@ import { fetchGroupApi } from "../../Api/Groups";
 import Clipboard from "../../assets/clipboard.svg";
 import { toast } from "react-toastify";
 import "./GroupInfo.scss";
+import { setDoc, doc } from "firebase/firestore";
+import { db } from "../../firebase";
 
+const divStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-around",
+  flexDirection: "column",
+  height: "200px",
+}
 export default function GroupInfo(props) {
   const { groupId } = useParams();
-  const [isToggledOn, handleToggleButton] = useState(true);
   const [group, setGroup] = useState();
 
   useEffect(() => {
@@ -25,7 +33,7 @@ export default function GroupInfo(props) {
       <>
         <button
           title="Copy to clipboard"
-          class="badge rounded-pill btn btn-success"
+          className="badge rounded-pill btn btn-success"
           onClick={() => {
             navigator.clipboard.writeText(group?.key);
             toast.success("Group key was copied to clipboard");
@@ -34,48 +42,44 @@ export default function GroupInfo(props) {
           {group?.key} &nbsp;
           <img src={Clipboard} alt="clip" />
         </button>
-        {/* <h6>Share this key with others to invite them to this group</h6> */}
       </>
     );
   };
 
-  function toggle() {
-    handleToggleButton(!isToggledOn);
+  const handleDoorStatus = async () => {
+  }
+
+  const handleDoorFreeze = async () => {
+    if (props.user.email !== group?.admin) {
+      toast.warning("You need admin access to perform this operation");
+      return;
+    }
+    // update on the firebase database
+    await setDoc(doc(db, "groups", groupId), { freeze: !group.freeze }, { merge: true })
+    // update on the current UI
+    setGroup({ ...group, freeze: !group.freeze })
   }
 
   return (
     <div id="bdy">
       <Header />
 
-      {/* 
-      <h4 className="mb-3">Status: {group?.status === true ? "Open" : "Closed"} | Freezed: {group?.freeze}</h4>
-       */}
-      <div style={{display:"flex", justifyContent:"space-around", alignItems:"center", marginTop:"10px"}}>
+      <div style={{ display: "flex", justifyContent: "space-around", alignItems: "center", marginTop: "10px" }}>
         <ShareKey />
-        <button className="bn62"><span>Freeze  <i class="fa-solid fa-lock"></i></span></button>
+        <button className="bn62" onClick={handleDoorFreeze}><span> {group?.freeze === true ? "Unfreeze" : "Freeze"} <i className="fa-solid fa-lock"></i></span></button>
       </div>
-      <h1 className="mb-3" style={{textAlign:"center", marginTop:"40px", color:"white"}}>{group?.name}</h1>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-around",
-          flexDirection: "column",
-          height: "200px",
-        }}
-      >
+
+      <h1 className="mb-3" style={{ textAlign: "center", marginTop: "40px", color: "white" }}>{group?.name}</h1>
+      
+      <div style={divStyle}>
         <button
-          className={"power_button " + (isToggledOn ? "is-active" : "")}
-          onClick={toggle}
+          className={"power_button " + (group?.status === true ? "is-active" : "")}
+          onClick={handleDoorStatus}
         >
-          <div className="power_button__icon">
-            <span className="power_button__icon__arrow"></span>
-          </div>
-          <p className="power_status">{isToggledOn ? "Locked" : "Unlocked"}</p>
+          <div className="power_button__icon"><span className="power_button__icon__arrow"></span></div>
+          <p className="power_status">{group?.status === false ? "Lock" : "Unlock"}</p>
         </button>
       </div>
-      {/* Controls */}
-      {/* History */}
     </div>
   );
 }
